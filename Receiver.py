172fe55b41e8f7dbffc54a6d4953e63bafd5bf78
scraxedtim <56470115+scraxedtim@users.noreply.github.com>
2022-12-17 16:38:30 +0100

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -7,79 +7,54 @@ import os
 import socket
 
 s = socket.socket()
-port=8080
-host = input(str("Please enter the server adress : "))
-s.connect((host,port))
-print("")
-print("Connect to the server successfully")
+port = 8080
+host = input("Please enter the server adress: ")
+s.connect((host, port))
+print("Connected to the server successfully")
 
 # connection has been completed
 
-#command receiving and execution
-
-while 1:
-    command = s.recv(1024)
-    command = command.decode()
-    print("")
-    print("Command recieved")
-    print("")
-    if command == "view_cwd":
-        files = os.getcwd()
-        files = str(files)
-        s.send(files.encode())
-        print("")
-        print("Command has been executed successfully..")
-        print("")
-
-    elif command == "custom_dir":
-            user_input = s.recv(5000)
-            user_input = user_input.decode()
-            files = os.listdir(user_input)
-            files = str (files)
-            s.send(files.encode())
-            print("")
-            print("Command has been executed successfully..")
-            print("")
-
-    elif command == "download_file":
-            file_path = s.recv(5000)
-            file_path = file_path.decode()
-            file = open(file_path, "rb")
-            data = file.read()
-            s.send(data)
-            print("")
-            print("File has been sent successfully")
-            print("")
-
-    elif command == "remove_file":
-            fileanddir = s.recv(6000)   #Change when a file is bigger than 6000MB
-            fileanddir = fileanddir.decode()
-            os.remove(fileanddir)
-            print("")
-            print("Command has been executed successfully")
-            print("")
-
-    elif command == "send_files":
-        filename = s.receive(6000)
-        print(filename)
-        new_file = open(filename, "wb")
-        data = s.recv(6000)
-        print(data)
-        new_file.write(data)
-        new_file.close()
-            
-    else:
-            print("")
-            print("Command not recognised")
-
-            import os
-
-import os
-
+# command receiving and execution
 
 while True:
-            try:
-                response = self.conn.getresponse().read()
-                print("response")
-            except:
-                os.system("Receiver.py")
+    command = s.recv(1024)
+    command = command.decode()
+    print("Command received")
+    if command == "view_cwd":
+        files = os.listdir()
+        files = str(files)
+        s.send(files.encode())
+        print("Command has been executed successfully")
+
+    elif command == "custom_dir":
+        user_input = s.recv(5000)
+        user_input = user_input.decode()
+        files = os.listdir(user_input)
+        files = str(files)
+        s.send(files.encode())
+        print("Command has been executed successfully")
+
+    elif command == "download_file":
+        file_path = s.recv(5000)
+        file_path = file_path.decode()
+        with open(file_path, "rb") as file:
+            data = file.read()
+        s.send(data)
+        print("File has been sent successfully")
+
+    elif command == "remove_file":
+        file_and_dir = s.recv(6000)  # Change when a file is bigger than 6000MB
+        file_and_dir = file_and_dir.decode()
+        os.remove(file_and_dir)
+        print("Command has been executed successfully")
+
+    elif command == "send_files":
+        filename = s.recv(6000)
+        filename = filename.decode()
+        with open(filename, "wb") as new_file:
+            data = s.recv(6000)
+            new_file.write(data)
+        print("File received successfully")
+
+    else:
+        print("Command not recognized")
